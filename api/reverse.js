a0eedac24ab4690a856f2e13f63d50d0cfb3b744
loris3758-gif <loris3758@gmail.com>
2026-01-21 19:06:38 +0100

    Update reverse.js

--- api/reverse.js
+++ api/reverse.js
@@ -1,4 +1,14 @@
-export default async function handler(req, res) {
+export default async function handler(req, res) { 
+  // Imposta header CORS generali
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Gestisci preflight OPTIONS
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   const { lat, lon } = req.query;
 
   if (!lat || !lon) {
@@ -15,6 +25,6 @@ export default async function handler(req, res) {
 
   const data = await response.json();
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json(data);
 }
+
